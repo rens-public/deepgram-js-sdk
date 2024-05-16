803552c36115a3fbef24e950f752773dd858bf20
Rens <rensbril@proton.me>
2024-05-16 15:16:32 +0200

    Update fetch.ts

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -11,7 +11,7 @@ export const resolveFetch = (customFetch?: Fetch): Fetch => {
   } else {
     _fetch = fetch;
   }
-  return (...args) => _fetch(...args);
+  return (...args: any[]) => _fetch(...args as [any]);
 };
 
 export const fetchWithAuth = (apiKey: string, customFetch?: Fetch): Fetch => {
